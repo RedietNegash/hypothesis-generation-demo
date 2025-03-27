5e04ec963b28fc53c224e1ae5e98b284d0d913da
Ofgeha-Gelana <ofgehagelana2019@gmail.com>
2025-03-27 03:51:13 +0300

    import libraries

---
+++ finemapping_susie_for_ALL_chr.py
@@ -0,0 +1,10 @@
+import gwaslab as gl
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+import gzip
+from rpy2.robjects.packages import importr
+import rpy2.robjects as ro
+import rpy2.robjects.numpy2ri as numpy2ri
+import rpy2.robjects.pandas2ri as pandas2ri
