e099c206856b6de66cf6dacd9655506aaa1632c6
Rediet Negash <redietnegash9@gmail.com>
2025-06-30 07:40:15 -0700

    ldsc for gtex datasets

---
+++ scripts/run_ldsc_gtex_all.sh
@@ -0,0 +1,19 @@
+
+SUMSTATS=../../data/heritablity/munged_body_BMIz.sumstats.gz
+WEIGHTS=../../data/heritablity/1000G_Phase3_weights_hm3_no_MHC/weights.hm3_noMHC.
+FRQ=../../data/heritablity/1000G_Phase3_frq/1000G.EUR.QC.
+OUTDIR=../../data/heritablity/gtex_ldsc_results/
+
+for i in {1..53}
+do
+  PREFIX=GTEx.$i
+  echo "Running LDSC for $PREFIX..."
+
+  python ldsc.py \
+    --h2 $SUMSTATS \
+    --ref-ld-chr ../../data/heritablity/Multi_tissue_gene_expr_1000Gv3_ldscores/${PREFIX}. \
+    --w-ld-chr $WEIGHTS \
+    --frqfile-chr $FRQ \
+    --overlap-annot \
+    --out ${OUTDIR}BMI_${PREFIX}_results
+done
