fd789ef2f66c6206dba53dec8e16229372244d70
Rediet Negash <redietnegash9@gmail.com>
2025-07-19 01:30:42 -0700

    update missing tissues from 42basepairs

---
+++ gene_expression/include_missing_tissues.py
@@ -0,0 +1,9 @@
+import pandas as pd
+
+
+df = pd.read_csv("gtex_tissue_mappings.tsv", sep="\t", header=None, names=["GTEx_tissue", "UBERON_ID"])
+new_row = pd.DataFrame([["Bladder", "UBERON_0001255"]], columns=["GTEx_tissue", "UBERON_ID"])
+df = pd.concat([df, new_row], ignore_index=True)
+
+
+df.to_csv("gtex_tissue_mappings_updated.tsv", sep="\t", index=False, header=False)
